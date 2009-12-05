9aa91b3f18ad9eda65273062417b05709934e90a
alshain <c.vonrueti@project-infcom.ch>
2009-12-05 16:47:27 +0000

    PingBot: Removed clutter, initialize direction

--- RobotMain/trunk/src/ch/nksa/pu/g2007e/pingbot/PingBot.java
+++ RobotMain/trunk/src/ch/nksa/pu/g2007e/pingbot/PingBot.java
@@ -2,6 +2,7 @@ package ch.nksa.pu.g2007e.pingbot;
 
 import ch.nksa.pu.g2007e.pingbot.SensorMount;
 import ch.nksa.pu.robotics.libs.BaseUnit;
+import ch.nksa.pu.robotics.libs.BaseUnit.Orientation;
 import lejos.nxt.*;
 
 public class PingBot {
@@ -10,13 +11,12 @@ public class PingBot {
 	 * @param args
 	 */
 	public static BaseUnit base = new BaseUnit();
-	public static SensorMount sMount = new SensorMount();
+	//public static SensorMount sMount = new SensorMount();
 	
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		base.setSpeed(8000);
-		base.spinLeft();
-		Thread.sleep(8000);
+		base.setOrientation(Orientation.BACKWARD);
+
 	}
 
 }
